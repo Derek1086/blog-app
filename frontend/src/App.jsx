f63545206aa49aa5caef2155f9f4ac64ef8cdd28
Derek Avila <derekavila1086@gmail.com>
2024-06-18 12:32:21 -0500

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,40 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import PostDetails from "./pages/PostDetails";
+import CreatePost from "./pages/CreatePost";
+import EditPost from "./pages/EditPost";
+import Profile from "./pages/Profile";
+import { UserContextProvider } from "./context/UserContext";
+import MyBlogs from "./pages/MyBlogs";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+const App = () => {
+  return (
+    <UserContextProvider>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/write" element={<CreatePost />} />
+          <Route exact path="/posts/post/:id" element={<PostDetails />} />
+          <Route exact path="/edit/:id" element={<EditPost />} />
+          <Route exact path="/myblogs/:id" element={<MyBlogs />} />
+          <Route exact path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </ThemeProvider>
+    </UserContextProvider>
+  );
+};
+
+export default App;
