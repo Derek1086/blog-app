0b541c7f54a479eb71512b3ba18f4f5e2127a5c6
Derek Avila <derekavila1086@gmail.com>
2024-06-19 10:42:52 -0500

    login prototype

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,10 @@
 import { Link, useNavigate } from "react-router-dom";
-import Footer from "../components/Footer";
 import { useContext, useState } from "react";
 import axios from "axios";
 import { URL } from "../url";
 import { UserContext } from "../context/UserContext";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -28,49 +29,55 @@ const Login = () => {
   };
   return (
     <>
-      <div className="flex items-center justify-between px-6 md:px-[200px] py-4">
-        <h1 className="text-lg md:text-xl font-extrabold">
-          <Link to="/">Blog Market</Link>
-        </h1>
-        <h3>
-          <Link to="/register">Register</Link>
-        </h3>
-      </div>
       <div className="w-full flex justify-center items-center h-[80vh] ">
         <div className="flex flex-col justify-center items-center space-y-4 w-[80%] md:w-[25%]">
           <h1 className="text-xl font-bold text-left">
             Log in to your account
           </h1>
-          <input
+          <TextField
             onChange={(e) => setEmail(e.target.value)}
-            className="w-full px-4 py-2 border-2 border-black outline-0"
-            type="text"
-            placeholder="Enter your email"
+            id="standard-basic"
+            label="Email"
+            variant="standard"
+            color="secondary"
+            style={{ width: "100%" }}
           />
-          <input
+          <TextField
             onChange={(e) => setPassword(e.target.value)}
-            className="w-full px-4 py-2 border-2 border-black outline-0"
+            id="standard-basic"
             type="password"
-            placeholder="Enter your password"
+            label="Password"
+            variant="standard"
+            color="secondary"
+            style={{ width: "100%" }}
           />
-          <button
+          <Button
             onClick={handleLogin}
-            className="w-full px-4 py-4 text-lg font-bold text-white bg-black rounded-lg hover:bg-gray-500 hover:text-black "
+            variant="contained"
+            color="secondary"
+            style={{ width: "100%", padding: "10px" }}
           >
             Log in
-          </button>
+          </Button>
           {error && (
             <h3 className="text-red-500 text-sm ">Something went wrong</h3>
           )}
           <div className="flex justify-center items-center space-x-3">
             <p>New here?</p>
-            <p className="text-gray-500 hover:text-black">
+            <p className="text-gray-500 hover:text-white">
               <Link to="/register">Register</Link>
             </p>
           </div>
+          <Button
+            onClick={() => navigate("/")}
+            variant="contained"
+            color="secondary"
+            style={{ width: "100%", padding: "10px" }}
+          >
+            Continue as Guest
+          </Button>
         </div>
       </div>
-      <Footer />
     </>
   );
 };
