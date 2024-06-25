2770a5d3be56f12c53cd2824fb954adb934759f3
Derek Avila <derekavila1086@gmail.com>
2024-06-25 11:07:27 -0500

    code cleanup

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -2,16 +2,12 @@ import { useContext } from "react";
 import { UserContext } from "../context/UserContext";
 import axios from "axios";
 import { URL } from "../url";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Box from "@mui/material/Box";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import Divider from "@mui/material/Divider";
-import InboxIcon from "@mui/icons-material/Inbox";
-import DraftsIcon from "@mui/icons-material/Drafts";
 
 const Menu = () => {
   const { user } = useContext(UserContext);
